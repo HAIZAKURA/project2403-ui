0c2625a2369a4a751e0489e15ca740f7a43d6957
HAIZAKURA <hhbilly99@gmail.com>
2024-04-07 15:25:33 +0900

    Create LoginView.vue

---
+++ src/views/LoginView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="login">
+    <el-row :gutter="20">
+      <el-col :span="6" :offset="6">
+        <el-card style="max-width: 480px"></el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script setup></script>
