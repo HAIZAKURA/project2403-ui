14affac910fccaf171e27678f0ae437bd66b3934
HAIZAKURA <hhbilly99@gmail.com>
2024-04-19 23:25:24 +0900

    Create LogView.vue

---
+++ src/views/LogView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="log">
+    <div class="title">
+      <span class="title-text">设&nbsp;备&nbsp;日&nbsp;志</span>
+    </div>
+  </div>
+</template>
+
+<style>
+.blank1 {
+  height: 1em;
+}
+
+.title {
+  text-align: left;
+}
+
+.title-text {
+  font-size: 1.5rem;
+}
+</style>
