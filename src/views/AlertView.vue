6c5b19fbdaee923ca769d31714628420cf4aaa13
HAIZAKURA <hhbilly99@gmail.com>
2024-04-19 23:25:17 +0900

    Create AlertView.vue

---
+++ src/views/AlertView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="alert">
+    <div class="title">
+      <span class="title-text">设&nbsp;备&nbsp;警&nbsp;报</span>
+    </div>
+  </div>
+</template>
+
+<style>
+.blank1 {
+  height: 1em;
+}
+
+.title {
+  text-align: left;
+}
+
+.title-text {
+  font-size: 1.5rem;
+}
+</style>
